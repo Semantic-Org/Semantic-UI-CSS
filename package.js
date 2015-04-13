2bb0fec6c5ecb2b62d17e87022693af51e896dc9
Pusher Robot <jack@semantic-ui.com>
2015-04-13 15:22:27 -0400

    Updated distribution to version 1.11.7

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ var
 Package.describe({
   name    : 'semantic:ui-css',
   summary : 'Semantic UI - CSS Release of Semantic UI',
-  version : '1.11.6',
+  version : '1.11.7',
   git     : 'git://github.com/Semantic-Org/Semantic-UI-CSS.git',
 });
 
