4b65000a66a99dc64d54359cc19ed548cf868fa1
Jack Lukic <jack@semantic-ui.com>
2017-08-07 09:18:56 -0700

    Updated distribution to version 2.2.13

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ var
 Package.describe({
   name    : 'semantic:ui-css',
   summary : 'Semantic UI - CSS Release of Semantic UI',
-  version : '2.2.12',
+  version : '2.2.13',
   git     : 'git://github.com/Semantic-Org/Semantic-UI-CSS.git',
 });
 
